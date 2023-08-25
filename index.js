1f6dbf3ddd04d079843057ea4a42f7ca3ec9603c
Maria Delacruz <mar23iaa@gmail.com>
2023-08-25 14:48:48 -0700

    updated my code

---
+++ index.js
@@ -0,0 +1,14 @@
+let homeScore = document.getElementById("homeScore");
+let guestScore = document.getElementById("guestScore")
+
+function add1() {
+
+}
+
+function add2() {
+
+}
+
+function add3() {
+
+}
