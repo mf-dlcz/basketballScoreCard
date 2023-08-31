25b22cdbb13e8a11ffbbb835b29c898f2b1f88eb
Maria Delacruz <mar23iaa@gmail.com>
2023-08-31 14:00:22 -0700

    Added a reset button, worked on the functionality of each button

--- index.js
+++ index.js
@@ -1,14 +1,45 @@
 let homeScore = document.getElementById("homeScore");
-let guestScore = document.getElementById("guestScore")
+let guestScore = document.getElementById("guestScore");
 
-function add1() {
+//Global scope variables
+let scoreCount = 0;
 
+const add1_homeScore = () => {
+  scoreCount += 1;
+  homeScore.textContent = scoreCount;
 }
 
-function add2() {
-
+const add2_homeScore = () => {
+  scoreCount += 2;
+  homeScore.textContent = scoreCount;
 }
 
-function add3() {
+const add3_homeScore = () => {
+  scoreCount += 3;
+  homeScore.textContent = scoreCount;
+}
 
+const add1_guestScore = () => {
+  scoreCount += 1;
+  guestScore.textContent = scoreCount;
+}
+
+const add2_guestScore = () => {
+  scoreCount += 2;
+  guestScore.textContent = scoreCount;
+}
+
+const add3_guestScore = () => {
+  scoreCount += 3;
+  guestScore.textContent = scoreCount;
+}
+
+const resetHomeScore = () => {
+  homeScore.textContent =  0;
+  scoreCount = 0;
+}
+
+const resetGuestScore = () => {
+  guestScore.textContent = 0;
+  scoreCount = 0;
 }
