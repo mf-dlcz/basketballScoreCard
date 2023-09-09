2c4429dea45aedf4dbe9cee138cc0a17d120e534
Maria Delacruz <mar23iaa@gmail.com>
2023-09-09 16:50:35 -0700

    upadted the HTML file and decided to call one function that will take care of adding 1, 2, or 3 to either the home or guest score board - still working on improving the logic. The reset button works perfectly clearing both scores with one click

--- index.js
+++ index.js
@@ -1,45 +1,24 @@
+//DOM manipulation
 let homeScore = document.getElementById("homeScore");
 let guestScore = document.getElementById("guestScore");
 
 //Global scope variables
-let scoreCount = 0;
+let homeScoreCount = 0;
+let guestScoreCount = 0;
 
-const add1_homeScore = () => {
-  scoreCount += 1;
-  homeScore.textContent = scoreCount;
+const addScore = () => {
+
+  homeScore.textContent = homeScoreCount;
+  guestScore.textContent = guestScoreCount;
 }
 
-const add2_homeScore = () => {
-  scoreCount += 2;
-  homeScore.textContent = scoreCount;
-}
 
-const add3_homeScore = () => {
-  scoreCount += 3;
-  homeScore.textContent = scoreCount;
-}
-
-const add1_guestScore = () => {
-  scoreCount += 1;
-  guestScore.textContent = scoreCount;
-}
-
-const add2_guestScore = () => {
-  scoreCount += 2;
-  guestScore.textContent = scoreCount;
-}
-
-const add3_guestScore = () => {
-  scoreCount += 3;
-  guestScore.textContent = scoreCount;
-}
-
-const resetHomeScore = () => {
+const resetScore = () => {
+  //resets homeScore
   homeScore.textContent =  0;
-  scoreCount = 0;
-}
+  homeScoreCount = 0;
 
-const resetGuestScore = () => {
+//resets guestScore
   guestScore.textContent = 0;
-  scoreCount = 0;
+  guestScoreCount = 0;
 }
